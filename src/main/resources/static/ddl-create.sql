7f4a6aa6d400496c1680cc56a89c132d4d435b15
Chris Innojam <40183367+cinnojam@users.noreply.github.com>
2018-06-26 16:42:53 -0500

    Add files via upload

--- src/main/resources/static/ddl-create.sql
+++ src/main/resources/static/ddl-create.sql
@@ -13,9 +13,10 @@ CREATE TABLE `security` (
   `issue` char(2) COLLATE utf8_bin NOT NULL,
   `issue_date` date NOT NULL,
   `maturity_date` date NOT NULL,
-  `desc` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+  `held_par` double NOT NULL DEFAULT '0',
+  `desc` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`cusip`),
-  UNIQUE KEY `issuer_issue_UNIQUE` (`issuer`,`issue`) /*!80000 INVISIBLE */
+  UNIQUE KEY `issuer_issue_UNIQUE` (`issuer`,`issue`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `price` (
@@ -26,9 +27,3 @@ CREATE TABLE `price` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `cusip_date_UNIQUE` (`cusip`,`date`) /*!80000 INVISIBLE */
 ) ENGINE=InnoDB AUTO_INCREMENT=7725 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-CREATE TABLE `portfolio` (
-  `cusip` char(6) COLLATE utf8_bin NOT NULL,
-  `par` double DEFAULT NULL,
-  PRIMARY KEY (`cusip`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
