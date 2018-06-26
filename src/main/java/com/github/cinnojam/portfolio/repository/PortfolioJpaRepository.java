6d39b29e24057000dabe9909b815396f110f8555
Chris Innojam <40183367+cinnojam@users.noreply.github.com>
2018-06-26 16:48:55 -0500

    Delete PortfolioJpaRepository.java

--- src/main/java/com/github/cinnojam/portfolio/repository/PortfolioJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.cinnojam.portfolio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.github.cinnojam.portfolio.model.Portfolio;
-
-public interface PortfolioJpaRepository extends JpaRepository<Portfolio, String> {
-
-}
